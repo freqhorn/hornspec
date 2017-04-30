d32dab18cf47ce2acab9f3d85cbb545f6556d077
Sam Kaufman <emrysk@gmail.com>
2017-04-29 19:12:33 -0700

    added --resume and --verbose to benchmark-others.py

--- tools/deep/benchmark-others.py
+++ tools/deep/benchmark-others.py
@@ -6,6 +6,7 @@ import sys
 import json
 import argparse
 import subprocess
+from collections import defaultdict
 
 
 TOTAL_TIME_RE = re.compile(r'\s*[tT]otal [tT]ime:?\s+([\.0-9]*)\s*')
@@ -16,12 +17,17 @@ class NoSuccessException(Exception):
     pass
 
 
+def getenv_or_raise(v):
+    r = os.getenv(v)
+    if r is None:
+        raise Exception("environment variable %s not set" % v)
+    return r
+
 # TODO: Note that /printAssignment and /trace option is for interactive runs and might cause a significant slowdown.
 def run_ice(example_path):
     """Returns ICE Boogie.exe on `example_path`. Returns runtime on success."""
     assert os.path.isfile(example_path)
-    root = os.getenv("ICE_ROOT")
-    assert root is not None
+    root = getenv_or_raise("ICE_ROOT")
     cmd = os.path.join(root, "Boogie", "Binaries-Full", "Boogie.exe")
     assert os.path.isfile(cmd)
     output = subprocess.check_output([cmd, "/nologo", "/noinfer",
@@ -35,8 +41,7 @@ def run_ice(example_path):
 
 
 def ice_benchmarks():
-    root = os.getenv("ICE_ROOT")
-    assert root is not None
+    root = getenv_or_raise("ICE_ROOT")
     bench_root = os.path.join(root, "benchmarks")
     assert os.path.isdir(bench_root)
     for root, dirs, files in os.walk(bench_root):
@@ -46,8 +51,7 @@ def ice_benchmarks():
 
 
 def run_mcmc(example_dirpath):
-    root = os.getenv("MCMC_ROOT")
-    assert root is not None
+    root = getenv_or_raise("MCMC_ROOT")
     cmd = os.path.join(root, "Release", "numerical.exe")
     assert os.path.isfile(cmd)
     output = subprocess.check_output([cmd], cwd=example_dirpath)
@@ -60,8 +64,7 @@ def run_mcmc(example_dirpath):
 
 def mcmc_benchmarks():
     # Gather all directories with .xml in 'em
-    root = os.getenv("MCMC_ROOT")
-    assert root is not None
+    root = getenv_or_raise("MCMC_ROOT")
     bench_root = os.path.join(root, "benchmarks", "numerical")
     assert os.path.isdir(bench_root)
     for root, dirs, files in os.walk(bench_root):
@@ -79,7 +82,8 @@ def bench_intersection(a, b):
     a_set = set(name_only(path) for path in a)
     b_set = set(name_only(path) for path in b)
     i = a_set.intersection(b_set)
-    return [x for x in a if name_only(x) in i], [x for x in b if name_only(x) in i]
+    return ([x for x in a if name_only(x) in i],
+            [x for x in b if name_only(x) in i])
 
 
 def main():
@@ -91,15 +95,24 @@ def main():
                         help="run the ICE (Boogie.exe) solver N iterations")
     parser.add_argument("-o", "--outfile", type=str,
                         help="path to file to save times")
+    parser.add_argument('-r', "--resume", action='store_true',
+                        help="merge results with outfile if it exists")
+    parser.add_argument('-v', "--verbose", action='store_true',
+                        help="be noisier about what's going on")
     args = parser.parse_args()
 
-    if args.outfile and os.path.exists(args.outfile):
+    if not args.resume and args.outfile and os.path.exists(args.outfile):
         print("'%s' already exists" % args.outfile, file=sys.stderr)
         return 1
 
-    try:
-        times, others = {}, []
+    times, others = {}, []
+    if args.resume and os.path.exists(args.outfile):
+        with open(args.outfile, 'r') as f:
+            times = json.load(f)['times']
+    times = defaultdict(lambda: defaultdict(list), {k: defaultdict(list, {a: b for a, b in v.iteritems()})
+                                                    for k, v in times.iteritems()})
 
+    try:
         # Construct name-only set of benchmarks to run
         if args.BENCHNAMES:
             lc_benchnames = set(l.lower() for l in args.BENCHNAMES)
@@ -112,7 +125,8 @@ def main():
             lc_benchnames = orig_sets[0]
             for s in orig_sets[1:]:
                 lc_benchnames.intersection_update(s)
-            print("Running benchmarks:", ', '.join(lc_benchnames))
+            if args.verbose:
+                print("Running benchmarks:", ', '.join(lc_benchnames))
 
         if args.mcmc:
             b = list(mcmc_benchmarks())
@@ -127,15 +141,19 @@ def main():
             b = [x for x in b if name_only(x) in lc_benchnames]
             others.append(("ICE", run_ice, args.ice, b))
         for n, fn, cnt, benches in others:
-            times[n] = {b: [fn(b) for _ in range(cnt)] for b in benches}
+            for b in benches:
+                t_list = times[n][name_only(b)]
+                while len(t_list) < cnt:
+                    if args.verbose:
+                        print("Running %s on %s" % (n, name_only(b)))
+                    t_list.append(fn(b))
     except KeyboardInterrupt:
         pass
 
     # Save the times
     if args.outfile:
         with open(args.outfile, 'w') as f:
-            cleaned = {k: {name_only(i): j for i, j in v.items()} for k, v in times.items()}
-            json.dump({'times': cleaned}, f)
+            json.dump({'times': times}, f)
 
     # Print the times
     for k, v in times.iteritems():
