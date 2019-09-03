e23db224f49a3a1396d5f5b77658a9052312e881
grigoryfedyukovich <grigory.fedyukovich@gmail.com>
2019-09-03 16:36:13 -0400

    an option for counterexamples; help message

--- tools/nonlin/NonlinSolver.cpp
+++ tools/nonlin/NonlinSolver.cpp
@@ -3,8 +3,30 @@
 using namespace ufo;
 using namespace std;
 
+static inline bool getBoolValue(const char * opt, bool defValue, int argc, char ** argv)
+{
+  for (int i = 1; i < argc; i++)
+  {
+    if (strcmp(argv[i], opt) == 0) return true;
+  }
+  return defValue;
+}
+
 int main (int argc, char ** argv)
 {
-  solveNonlin(string(argv[argc-1]));
+  if (getBoolValue("--help", false, argc, argv) || argc == 1){
+    outs () <<
+        "* * *                              FreqHorn-Nonlin v.0.1 - Copyright (C) 2019                              * * *\n" <<
+        "                                          Grigory Fedyukovich et al                                      \n\n" <<
+        "Usage:                          Purpose:\n" <<
+        " freqn [--help]                   show help\n" <<
+        " freqn [options] <file.smt2>      discover invariants for a system of constrained Horn clauses\n\n" <<
+        "Options:\n" <<
+        "  --cex                           search for counterexamples                         \n";
+
+    return 0;
+  }
+  bool cex = getBoolValue("--cex", false, argc, argv);
+  solveNonlin(string(argv[argc-1]), !cex);
   return 0;
 }
